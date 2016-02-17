07d9b201b6522ffea7131030cdc37e4421187c5c
ilandy <ilandy@infbeyond.com>
2016-02-17 17:17:38 +0800

    1. complete product gallery

--- javascripts/ctrl.js
+++ javascripts/ctrl.js
@@ -4,41 +4,57 @@
 	angular
 		.module('myApp')
 		.controller('myCtrl',myCtrl);
-
+	
 	function myCtrl(){
-
 		var vm = this;
-		vm.isActive = false,
-		vm.item = productInfo,
-		vm.pType = 0,
-		vm.pData = {},
-		vm.szData = szInfo,
+		vm.isActive = false;
+		vm.item = proType1.concat(proType2).concat(proType3).concat(proType4);
+		vm.item1 = proType1;
+		vm.item2 = proType2;
+		vm.item3 = proType3;
+		vm.item4 = proType4;
+		vm.pType = 0;
+		vm.pData = {};
+		vm.szData = szInfo;
 		vm.szImg = vm.szData[0].Pic;
 
+		vm.seeMore = seeMore;
+		vm.showSZ = showSZ;
+		vm.menuActive = menuActive;
+		vm.getFloor = getFloor;
+		vm.ProConVisble = ProConVisble;
 
-		vm.seeMore = function(idx){
+		function seeMore(idx){
 			vm.pData = {
 				"Name" : vm.item[idx].Name,
 				"Pic": vm.item[idx].Pic,
 			 	"Discription": vm.item[idx].Discription,
 			 	"OtherImg": vm.item[idx].OtherImg,
+			 	"ProConVisble" : true ,
 			 	"ProContent": function(){
 			 		if(vm.item[idx].OtherImg.length !== 0){
 			 			return 'col-md-5';
 			 		}else{
-			 			return 'col-md-12';
+			 			return 'col-md-12 two-row';
 			 		}
-			 	}
+			 	},
+			 	"ContentIndex": Math.floor(idx / 3)
 			}
+
 		}
-		vm.showSZ = function(idx){
+		function showSZ (idx){
 			vm.szImg = vm.szData[idx].Pic;
 		 }
-		vm.menuActive = function(){
+		function menuActive (){
 			vm.isActive = !vm.isActive;
 		}
+		function getFloor (num){
+			return Math.floor(num);
+		}
+		function ProConVisble (clkCtrl,idx){
+			if(clkCtrl == true && vm.pData.ContentIndex == vm.getFloor(idx/3)){
+				return true
+			}
+		}
 	}
-
-		
-		
 })();
--- javascripts/data.js
+++ javascripts/data.js
@@ -1,199 +1,173 @@
-szInfo = [
-			{
-				"title": "信息化应用企业证书",
-				"Pic": "zs_1.gif"
-			},
-			{
-				"title": "安全生产标准化三级企业证书",
-				"Pic": "zs_2.gif"
-			},
-			{
-				"title": "纳税信用等级A级企业证书",
-				"Pic": "zs_3.gif"
-			},
-			{
-				"title": "农业产业化重点龙头企业证书",
-				"Pic": "zs_4.gif"
-			},
-			{
-				"title": "ISO9001认证证书",
-				"Pic": "zs_5.gif"
-			}
-		];
-productInfo = [
-		 	{
-		 		"Name": "歧化松香",
-		 		"ProType": 2,
-			 	"Pic": "20140822103015602.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "英文名：Disproportionated rosin<br>所属类别：松香及其衍生物<br>组成：<br>化学结构：<br>包装方式：镀锌铁桶装，每桶歧化松香净重225Kg，贮运时要注意防火防热。<br>上游产品：<br>下游产品：歧化松香酸皂、橡胶增塑剂，粘结剂、口香糖，高级造纸胶料，印刷油墨等。<br>",
-			 	 },
-			 	 { "title": "技术指标",
-			 	   "content": "歧化松香分为优级和一级，各级质量指标须符合下表要求<br><table class=\"product-table\"><tbody><tr><td class=\"text-left\">指标名称/指标/等级</td><td>特级</td><td>一级</td></tr><tr><td class=\"text-left\">颜色，不深于松香色级</td><td>一级</td><td>三级</td></tr><tr><td class=\"text-left\">枞酸含量（％），不小于</td><td>0.1</td><td>0.5</td></tr><tr><td class=\"text-left\">去氢枞酸含量（％），不小于</td><td>52.0</td><td>45.0</td></tr><tr><td class=\"text-left\">软化点（环球法）（℃），不小于</td><td>75.0</td><td>75.0</td></tr><tr><td class=\"text-left\">酸价（mgKOH/g）,不小于</td><td>155.0</td><td>150.0</td></tr><tr><td class=\"text-left\">不皂化物含量（％），不大于</td><td>10.0</td><td>12.0</td></tr></tbody></table>",
-			 	 },
-			 	 { "title": "規格",
-				   "content": "<table class=\"product-table\"><tbody><tr><td class=\"text-left\">指 标 名 称</td><td>特 级 品</td><td>一 级 品</td></tr><tr><td class=\"text-left\">颜色，罗维邦色号 ≤ 黄</td><td>20</td><td>40</td></tr><tr><td class=\"text-left\">红</td><td>2.1</td><td>3.4</td></tr><tr><td class=\"text-left\">枞酸含量，% ≤</td><td>0.1</td><td>0.5</td></tr><tr><td class=\"text-left\">脱氢枞酸含量% ≥</td><td>52.0</td><td>45.0</td></tr><tr><td class=\"text-left\">软化点（环球法）,℃ ≥</td><td>75.0</td><td>75.0</td></tr><tr><td class=\"text-left\">酸值，mgKOH/g ≥</td><td>155.0</td><td>150.0</td></tr><tr><td class=\"text-left\">不皂化物含量，% ≤</td><td>10.0</td><td>12.0</td></tr></tbody></table>" 
-			 	 },
-			 	 { "title": "安全措施",
-			 	   "content": "贮存运输时要注意防火防热，不可与自燃物、强氧化剂、强酸共储混运，按易燃物品规定处理。"
-				 },
-			 	 { "title": "用途",
-			 	   "content": "用于制造歧化松香酸皂，作为丁苯橡胶，氯丁橡胶，丁腈胶及其乳胶，ABS等高分子聚合的乳化剂，还可用作橡胶增塑剂，粘结剂、口香糖，高级造纸胶料，印刷油墨等。"
-				 },
-			 	 { "title": "性质",
-			 	   "content": "外观：微黄至黄红色带有蓝紫色荧光的透明硬脆玻璃状固体。<br>折光率：1.5400（）<br>闪点：约216℃（开杯）<br>着火点：480-500℃<br>沸点：265℃（15mmHg）"	
-			 	 },
-			 	 { "title": "包装、贮运、年产量",
-			 	   "content": "包装：0.3-0.5mm镀锌桶，225Kg/桶<br>贮运：避免日光直射、雨淋，在低温下密封保存。"	
-			 	 },
-			 	 { "title": "物理化学性质",
-			 	   "content": "岐化松香主要用作合成橡胶的聚合乳化剂，还大量用于制造水溶性压敏胶粘剂，也可作为橡胶组分的拼料，胶粘剂的增粘剂等。"	
-			 	 }
-			 	],
-			 	"OtherImg": ["20140826085608875.jpg","20140826085608876.jpg"]
-		 	},
-			{
-		 		"Name": "歧化松香钾皂", //產品名稱
-		 		"ProType": 2, //產品類別
-			 	"Pic": "20140822102029183.jpg", //產品圖片
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
+szInfo = [{
+    "title": "信息化应用企业证书",
+    "Pic": "zs_1.gif"
+}, {
+    "title": "安全生产标准化三级企业证书",
+    "Pic": "zs_2.gif"
+}, {
+    "title": "纳税信用等级A级企业证书",
+    "Pic": "zs_3.gif"
+}, {
+    "title": "农业产业化重点龙头企业证书",
+    "Pic": "zs_4.gif"
+}, {
+    "title": "ISO9001认证证书",
+    "Pic": "zs_5.gif"
+}];
+proType1 = [
+    {
+	    "Name": "精制松香",
+	    "ProType": 1,
+	    "Pic": "20140822111527217-1.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "暫無資料"
+	    }],
+	    "OtherImg": ["123.jpg", "124.jpg"]
+	}, {
+	    "Name": "脂松香",
+	    "ProType": 1,
+	    "Pic": "20140822101309800.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "暫無資料"
+	    }],
+	    "OtherImg": ["123.jpg", "124.jpg"]
+	}, {
+	    "Name": "顺酐改性松香",
+	    "ProType": 1,
+	    "Pic": "20140822101603361.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "暫無資料"
+	    }],
+	    "OtherImg": ["123.jpg", "124.jpg"]
+	}, {
+	    "Name": "富马改性松香",
+	    "ProType": 1,
+	    "Pic": "20140822101749914.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "暫無資料"
+	    }],
+	    "OtherImg": ["123.jpg", "124.jpg"]
+	}, {
+	    "Name": "造纸施胶剂专用改性松香",
+	    "ProType": 1,
+	    "Pic": "20140822160913271.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "暫無資料"
+	    }],
+	    "OtherImg": ["123.jpg", "124.jpg"]
+}];
 
-			 	 }], //產品說明
-			 	"OtherImg": ["123.jpg","124.jpg"] //產品說明的圖片
-		 	},
-		 	{
-		 		"Name": "松香树脂",
-		 		"ProType": 3,
-			 	"Pic": "20140822104039699.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
+proType2 = [
+	  {
+	    "Name": "歧化松香",
+	    "ProType": 2,
+	    "Pic": "20140822103015602.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "英文名：Disproportionated rosin<br>所属类别：松香及其衍生物<br>包装方式：镀锌铁桶装，每桶歧化松香净重225Kg，贮运时要注意防火防热。<br>下游产品：歧化松香酸皂、橡胶增塑剂，粘结剂、口香糖，高级造纸胶料，印刷油墨等。<br>",
+	    }, {
+	        "title": "技术指标",
+	        "content": "歧化松香分为优级和一级，各级质量指标须符合下表要求<br><br><table class=\"product-table\"><tbody><tr><td class=\"text-left\">指标名称/指标/等级</td><td>特级</td><td>一级</td></tr><tr><td class=\"text-left\">颜色，不深于松香色级</td><td>一级</td><td>三级</td></tr><tr><td class=\"text-left\">枞酸含量（％），不小于</td><td>0.1</td><td>0.5</td></tr><tr><td class=\"text-left\">去氢枞酸含量（％），不小于</td><td>52.0</td><td>45.0</td></tr><tr><td class=\"text-left\">软化点（环球法）（℃），不小于</td><td>75.0</td><td>75.0</td></tr><tr><td class=\"text-left\">酸价（mgKOH/g）,不小于</td><td>155.0</td><td>150.0</td></tr><tr><td class=\"text-left\">不皂化物含量（％），不大于</td><td>10.0</td><td>12.0</td></tr></tbody></table><br><br>",
+	    }, {
+	        "title": "規格",
+	        "content": "<table class=\"product-table\"><tbody><tr><td class=\"text-left\">指 标 名 称</td><td>特 级 品</td><td>一 级 品</td></tr><tr><td class=\"text-left\">颜色，罗维邦色号 ≤ 黄</td><td>20</td><td>40</td></tr><tr><td class=\"text-left\">红</td><td>2.1</td><td>3.4</td></tr><tr><td class=\"text-left\">枞酸含量，% ≤</td><td>0.1</td><td>0.5</td></tr><tr><td class=\"text-left\">脱氢枞酸含量% ≥</td><td>52.0</td><td>45.0</td></tr><tr><td class=\"text-left\">软化点（环球法）,℃ ≥</td><td>75.0</td><td>75.0</td></tr><tr><td class=\"text-left\">酸值，mgKOH/g ≥</td><td>155.0</td><td>150.0</td></tr><tr><td class=\"text-left\">不皂化物含量，% ≤</td><td>10.0</td><td>12.0</td></tr></tbody></table>"
+	    }, {
+	        "title": "安全措施",
+	        "content": "贮存运输时要注意防火防热，不可与自燃物、强氧化剂、强酸共储混运，按易燃物品规定处理。"
+	    }, {
+	        "title": "用途",
+	        "content": "用于制造歧化松香酸皂，作为丁苯橡胶，氯丁橡胶，丁腈胶及其乳胶，ABS等高分子聚合的乳化剂，还可用作橡胶增塑剂，粘结剂、口香糖，高级造纸胶料，印刷油墨等。"
+	    }, {
+	        "title": "性质",
+	        "content": "外观：微黄至黄红色带有蓝紫色荧光的透明硬脆玻璃状固体。<br>折光率：1.5400（）<br>闪点：约216℃（开杯）<br>着火点：480-500℃<br>沸点：265℃（15mmHg）"
+	    }, {
+	        "title": "包装、贮运、年产量",
+	        "content": "包装：0.3-0.5mm镀锌桶，225Kg/桶<br>贮运：避免日光直射、雨淋，在低温下密封保存。"
+	    }, {
+	        "title": "物理化学性质",
+	        "content": "岐化松香主要用作合成橡胶的聚合乳化剂，还大量用于制造水溶性压敏胶粘剂，也可作为橡胶组分的拼料，胶粘剂的增粘剂等。"
+	    }],
+	    "OtherImg": []
+	}, 
+	{
+	    "Name": "歧化松香钾皂",
+	    "ProType": 2,
+	    "Pic": "20140822102029183.jpg",
+	    "Discription": [{
+	        "title": "",
+	        "content": "暫無資料"
+	    }],
+	    "OtherImg": ["20140826085608875.jpg", "20140826085608876.jpg"]
+	}];
 
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "精制松香",
-		 		"ProType": 1,
-			 	"Pic": "20140822111527217-1.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "脂松香",
-		 		"ProType": 1,
-			 	"Pic": "20140822101309800.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "马林酸树脂",
-		 		"ProType": 3,
-			 	"Pic": "20140822103209363.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "松香季戊四醇脂",
-		 		"ProType": 3,
-			 	"Pic": "20140822103840428.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "松香甘油脂",
-		 		"ProType": 3,
-			 	"Pic": "20140822103349393.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "重油",
-		 		"ProType": 4,
-			 	"Pic": "20140822160258818.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "松节油－湿地松",
-		 		"ProType": 4,
-			 	"Pic": "20140822160522473.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "松节油－马尾松",
-		 		"ProType": 4,
-			 	"Pic": "20140822160706914.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "顺酐改性松香",
-		 		"ProType": 1,
-			 	"Pic": "20140822101603361.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "富马改性松香",
-		 		"ProType": 1,
-			 	"Pic": "20140822101749914.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	},
-		 	{
-		 		"Name": "造纸施胶剂专用改性松香",
-		 		"ProType": 1,
-			 	"Pic": "20140822160913271.jpg",
-			 	"Discription": [
-			 	 { "title": "",
-			 	   "content": "",
-
-			 	 }],
-			 	"OtherImg": ["123.jpg","124.jpg"]
-		 	}
-	 	];
+proType3 = [
+{
+    "Name": "松香树脂",
+    "ProType": 3,
+    "Pic": "20140822104039699.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}, {
+    "Name": "马林酸树脂",
+    "ProType": 3,
+    "Pic": "20140822103209363.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}, {
+    "Name": "松香季戊四醇脂",
+    "ProType": 3,
+    "Pic": "20140822103840428.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}, {
+    "Name": "松香甘油脂",
+    "ProType": 3,
+    "Pic": "20140822103349393.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}];
+proType4 = [{
+    "Name": "重油",
+    "ProType": 4,
+    "Pic": "20140822160258818.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}, {
+    "Name": "松节油－湿地松",
+    "ProType": 4,
+    "Pic": "20140822160522473.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}, {
+    "Name": "松节油－马尾松",
+    "ProType": 4,
+    "Pic": "20140822160706914.jpg",
+    "Discription": [{
+        "title": "",
+        "content": "暫無資料"
+    }],
+    "OtherImg": ["123.jpg", "124.jpg"]
+}];
