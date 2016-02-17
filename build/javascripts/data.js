938302eff5bc37023f3d89600e012a0443afa4b9
ilandy <ilandy@infbeyond.com>
2016-02-17 17:10:58 +0800

    1. complete product gallery 2. push middleman source code

---
+++ build/javascripts/data.js
@@ -0,0 +1,122 @@
+szInfo = [
+			{
+				"title": "信息化应用企业证书",
+				"Pic": "zs_1.gif"
+			},
+			{
+				"title": "安全生产标准化三级企业证书",
+				"Pic": "zs_2.gif"
+			},
+			{
+				"title": "纳税信用等级A级企业证书",
+				"Pic": "zs_3.gif"
+			},
+			{
+				"title": "农业产业化重点龙头企业证书",
+				"Pic": "zs_4.gif"
+			},
+			{
+				"title": "ISO9001认证证书",
+				"Pic": "zs_5.gif"
+			}
+		];
+productInfo = [
+		 	{
+		 		"Name": "歧化松香",
+		 		"ProType": 2,
+			 	"Pic": "20140822103015602.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["20140826085608875.jpg","20140826085608876.jpg"]
+		 	},
+			{
+		 		"Name": "歧化松香钾皂",
+		 		"ProType": 2,
+			 	"Pic": "20140822102029183.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "松香树脂",
+		 		"ProType": 3,
+			 	"Pic": "20140822104039699.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "精制松香",
+		 		"ProType": 1,
+			 	"Pic": "20140822111527217-1.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "脂松香",
+		 		"ProType": 1,
+			 	"Pic": "20140822101309800.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "马林酸树脂",
+		 		"ProType": 3,
+			 	"Pic": "20140822103209363.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "松香季戊四醇脂",
+		 		"ProType": 3,
+			 	"Pic": "20140822103840428.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "松香甘油脂",
+		 		"ProType": 3,
+			 	"Pic": "20140822103349393.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "重油",
+		 		"ProType": 4,
+			 	"Pic": "20140822160258818.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "松节油－湿地松",
+		 		"ProType": 4,
+			 	"Pic": "20140822160522473.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "松节油－马尾松",
+		 		"ProType": 4,
+			 	"Pic": "20140822160706914.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "顺酐改性松香",
+		 		"ProType": 1,
+			 	"Pic": "20140822101603361.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "富马改性松香",
+		 		"ProType": 1,
+			 	"Pic": "20140822101749914.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	},
+		 	{
+		 		"Name": "造纸施胶剂专用改性松香",
+		 		"ProType": 1,
+			 	"Pic": "20140822160913271.jpg",
+			 	"Discription": "infomation",
+			 	"OtherImg": ["123.jpg","124.jpg"]
+		 	}
+	 	];
